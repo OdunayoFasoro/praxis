6d6071a336bb4b2ae11b0ed78bd7f3f3bd9ff3e6
OdunayoFasoro <oofasoro@gmail.com>
2022-11-26 15:47:01 +0000

    new stuff

--- practice1.sh
+++ practice1.sh
@@ -1,9 +1,11 @@
-# this scrip is soley made for practice 
-# i hope i get ut because this git is a little bit confusing 
-# i refusse to entertain doubt 
+# this script is soley made for practice 
+# i hope i get it because this git is a little bit confusing 
+# i refuse to entertain doubt 
 # I CAN DO IT 
 # author Odunayomimare 
 echo "go lil rockstar"
 echo "look mama! i made it "
 echo "thanks so much dear Jesus"
+echo "why is it not pushing"
+echo "push my darling script, PUSH!!!!!"
 
